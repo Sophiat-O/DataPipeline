b14020a6dc9b3e66e3fa6f43ce1797a93a03628a
Sophiat-O <lilsylph@gmail.com>
2023-08-13 19:48:23 +0200

    create data_extraction_api

---
+++ data_app/orm_read.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import Session
+
+
+def get_instance(db, model, skip: int = 0, limit: int = 100):
+    instance = db.query(model).offset(skip).limit(limit).all()
+    return instance
