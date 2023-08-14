3bf95114e1e47b67554437a385799059930d95fc
Sophiat-O <lilsylph@gmail.com>
2023-08-14 16:43:03 +0200

    cleanup folder

--- data_app/orm_read.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-
-
-def get_instance(db, model, skip: int = 0, limit: int = 100):
-    instance = db.query(model).offset(skip).limit(limit).all()
-    return instance
